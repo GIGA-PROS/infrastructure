0916a4735a83b147cb45179434809708b7e02916
your user name <mcs1901@outlook.com>
2024-10-20 14:42:38 -0300

    make deploy readable

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-nix run nixpkgs#nixos-rebuild switch -- --flake .#kanagawa --target-host deploy@ec2-44-242-223-134.us-west-2.compute.amazonaws.com --fast --use-remote-sudo
+nix run nixpkgs#nixos-rebuild switch \
+    -- --flake .#kanagawa \
+    --target-host deploy@ec2-44-242-223-134.us-west-2.compute.amazonaws.com \
+    --fast \
+    --use-remote-sudo
