b5873f4c9aba47a787ee97209acf8e5016b5b723
your user name <mcs1901@outlook.com>
2024-10-25 22:04:18 -0300

    progress

--- saas/frappe.sh
+++ saas/frappe.sh
@@ -1,52 +1,56 @@
-#!bin/bash
+#!/bin/bash
 
-if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
-    echo "Bench already exists, skipping init"
-    cd frappe-bench
-    bench start
-else
-    echo "Creating new bench..."
-fi
+# Check if bench already exists
 
-export PATH="${NVM_DIR}/versions/node/v${NODE_VERSION_DEVELOP}/bin/:${PATH}"
+echo "Creating new bench..."
 
+# Initialize bench without Redis
 bench init --skip-redis-config-generation frappe-bench
 
 cd frappe-bench
 
-# Use containers instead of localhost
-# bench set-mariadb-host mariadb
+# Set PostgreSQL and Redis configurations
 bench set-postgres-host postgres
-bench set-redis-cache-host redis:6379
-bench set-redis-queue-host redis:6379
-bench set-redis-socketio-host redis:6379
+bench set-redis-cache-host redis
+bench set-redis-queue-host redis
+bench set-redis-socketio-host redis
 
-# Remove redis, watch from Procfile
-sed -i '/redis/d' ./Procfile
-sed -i '/watch/d' ./Procfile
+# Remove Redis and watch processes from Procfile
+sed -i '/redis/d' Procfile
+sed -i '/watch/d' Procfile
 
+# Get the LMS and CRM apps
 bench get-app lms
 bench get-app crm
 
-# bench new-site edu.gigapros.io \
-# --force \
-# --mariadb-root-password 123 \
-# --admin-password admin \
-# --no-mariadb-socket
-
+# Create the edu.gigapros.io site and install the LMS app
 bench new-site edu.gigapros.io \
     --force \
     --admin-password admin \
-    --db-root-username postgres \
-    --db-root-password postgres \
     --db-type postgres \
+    --db-host postgres \
+    --db-port 5432 \
+    --db-name edu_gigapros_io \
+    --db-root-username giga \
+    --db-root-password giga
 
 bench --site edu.gigapros.io install-app lms
 bench --site edu.gigapros.io set-config developer_mode 1
 bench --site edu.gigapros.io clear-cache
+
+# Create the crm.gigapros.io site and install the CRM app
+bench new-site crm.gigapros.io \
+    --force \
+    --admin-password admin \
+    --db-type postgres \
+    --db-host postgres \
+    --db-port 5432 \
+    --db-name crm_gigapros_io \
+    --db-root-username giga \
+    --db-root-password giga
+
 bench --site crm.gigapros.io install-app crm
 
-bench use edu.gigapros.io
-bench use crm.gigapros.io
 
+# Start bench
 bench start
