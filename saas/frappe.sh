574d23d7bc46f7b829b307530ab6e3c9377b275f
your user name <mcs1901@outlook.com>
2024-10-25 21:49:41 -0300

    refactor

--- saas/frappe.sh
+++ saas/frappe.sh
@@ -15,7 +15,8 @@ bench init --skip-redis-config-generation frappe-bench
 cd frappe-bench
 
 # Use containers instead of localhost
-bench set-mariadb-host mariadb
+# bench set-mariadb-host mariadb
+bench set-postgres-host postgres
 bench set-redis-cache-host redis:6379
 bench set-redis-queue-host redis:6379
 bench set-redis-socketio-host redis:6379
@@ -27,18 +28,25 @@ sed -i '/watch/d' ./Procfile
 bench get-app lms
 bench get-app crm
 
-bench new-site lms.localhost \
---force \
---mariadb-root-password 123 \
---admin-password admin \
---no-mariadb-socket
+# bench new-site edu.gigapros.io \
+# --force \
+# --mariadb-root-password 123 \
+# --admin-password admin \
+# --no-mariadb-socket
 
-bench --site lms.localhost install-app lms
-bench --site lms.localhost set-config developer_mode 1
-bench --site lms.localhost clear-cache
-bench --site crm.localhost install-app crm
+bench new-site edu.gigapros.io \
+    --force \
+    --admin-password admin \
+    --db-root-username postgres \
+    --db-root-password postgres \
+    --db-type postgres \
 
-bench use lms.localhost
-bench use crm.localhost
+bench --site edu.gigapros.io install-app lms
+bench --site edu.gigapros.io set-config developer_mode 1
+bench --site edu.gigapros.io clear-cache
+bench --site crm.gigapros.io install-app crm
+
+bench use edu.gigapros.io
+bench use crm.gigapros.io
 
 bench start
